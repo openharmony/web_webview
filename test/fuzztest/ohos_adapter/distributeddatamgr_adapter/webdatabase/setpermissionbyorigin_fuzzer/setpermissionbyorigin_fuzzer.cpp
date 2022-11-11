f412c7b651796df455d0a08e83db93154387f1ca
zhouliting <zhouliting5@huawei.com>
2022-11-11 17:31:21 +0800

    Add FUZZ Test Case
    Signed-off-by: zhouliting <zhouliting5@huawei.com>

---
+++ test/fuzztest/ohos_adapter/distributeddatamgr_adapter/webdatabase/setpermissionbyorigin_fuzzer/setpermissionbyorigin_fuzzer.cpp
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "setpermissionbyorigin_fuzzer.h"
+
+#include <cstring>
+#include <securec.h>
+
+#include "ohos_web_permission_data_base_adapter_impl.h"
+
+using namespace OHOS::NWeb;
+
+namespace OHOS {
+    bool DoSomethingInterestingWithMyAPI(const uint8_t* data, size_t size)
+    {
+        if ((data == nullptr) || (size == 0)) {
+            return false;
+        }
+        std::string origin((const char *)data, size);
+        WebPermissionType key = WebPermissionType::GEOLOCATION;
+        bool result = true;
+        OhosWebPermissionDataBaseAdapterImpl::GetInstance().SetPermissionByOrigin(origin, key, result);
+        return true;
+    }
+}
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::DoSomethingInterestingWithMyAPI(data, size);
+    return 0;
+}
