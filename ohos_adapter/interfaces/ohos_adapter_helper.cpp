d10159e751d2c7a7d540ac2a4b1fdeb06dcff5f0
zgit2021 <zhaowenqiang14@huawei.com>
2023-02-24 21:45:45 +0800

    Web add inputmethod framework adaptation layer cherry to monthly
    Signed-off-by: zgit2021 <zhaowenqiang14@huawei.com>
    Change-Id: Ia48df546049d1f5c94fbcb06250344dbf7fb9a4a

--- ohos_adapter/interfaces/ohos_adapter_helper.cpp
+++ ohos_adapter/interfaces/ohos_adapter_helper.cpp
@@ -19,7 +19,9 @@
 #include "audio_renderer_adapter_impl.h"
 #include "audio_system_manager_adapter_impl.h"
 #include "battery_mgr_client_adapter_impl.h"
+#include "datashare_adapter_impl.h"
 #include "display_manager_adapter_impl.h"
+#include "imf_adapter_impl.h"
 #include "mmi_adapter_impl.h"
 #include "net_connect_adapter_impl.h"
 #include "ohos_init_web_adapter_impl.h"
@@ -29,7 +31,6 @@
 #include "pasteboard_client_adapter_impl.h"
 #include "power_mgr_client_adapter_impl.h"
 #include "soc_perf_client_adapter_impl.h"
-#include "datashare_adapter_impl.h"
 
 namespace OHOS::NWeb {
 // static
@@ -112,5 +113,10 @@ std::unique_ptr<OhosInitWebAdapter> OhosAdapterHelper::GetInitWebAdapter() const
 DatashareAdapter& OhosAdapterHelper::GetDatashareInstance() const
 {
     return DatashareAdapterImpl::GetInstance();
+}
+
+std::unique_ptr<IMFAdapter> OhosAdapterHelper::CreateIMFAdapter() const
+{
+    return std::make_unique<IMFAdapterImpl>();
 }
 } // namespace OHOS::NWeb
