02f1d5ff5c5891539488e77848f17bae5ef94ea2
zhouzhiming <zhouzhiming6@huawei.com>
2024-09-05 17:17:33 +0800

    heif图片解码接口适配
    Signed-off-by: zhouzhiming <zhouzhiming6@huawei.com>

--- ohos_adapter/ohos_adapter_helper/src/ohos_adapter_helper_impl.cpp
+++ ohos_adapter/ohos_adapter_helper/src/ohos_adapter_helper_impl.cpp
@@ -354,7 +354,6 @@ std::unique_ptr<MediaAVSessionAdapter> OhosAdapterHelperImpl::CreateMediaAVSessi
 
 std::unique_ptr<OhosImageDecoderAdapter> OhosAdapterHelperImpl::CreateOhosImageDecoderAdapter()
 {
-    PRINT_NOT_IMPL_FUNC_LOG();
     return std::make_unique<OhosImageDecoderAdapterImpl>();
 }
 
