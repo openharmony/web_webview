53f726829700901d26eedaf2f1e666a85e83d811
胡文韬 <huwentao21@huawei.com>
2024-09-09 10:56:34 +0000

    蓝黄一致性release
    Signed-off-by: 胡文韬 <huwentao21@huawei.com>

--- ohos_interface/ohos_glue/ohos_adapter/bridge/webcore/ark_cert_manager_adapter_wrapper.h
+++ ohos_interface/ohos_glue/ohos_adapter/bridge/webcore/ark_cert_manager_adapter_wrapper.h
@@ -45,7 +45,7 @@ public:
 
     int32_t GetCertDataBySubject(const char* subjectName, uint8_t* certData, int32_t certType) override;
 
-    int VerifyCertFromNetSsl(uint8_t* certData, uint32_t certSize);
+    int VerifyCertFromNetSsl(uint8_t* certData, uint32_t certSize) override;
 
     bool GetTrustAnchorsForHostName(const std::string& hostname, std::vector<std::string>& certs) override;
 
