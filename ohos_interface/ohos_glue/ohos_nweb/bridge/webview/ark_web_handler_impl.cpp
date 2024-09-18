39e0cf603e07d50a6aadc69574a69ea441e3497e
jiangkuaixue <jiangchenzhou@huawei.com>
2024-09-18 11:40:00 +0800

    add color picker
    Signed-off-by: jiangkuaixue <jiangchenzhou@huawei.com>

--- ohos_interface/ohos_glue/ohos_nweb/bridge/webview/ark_web_handler_impl.cpp
+++ ohos_interface/ohos_glue/ohos_nweb/bridge/webview/ark_web_handler_impl.cpp
@@ -17,6 +17,7 @@
 
 #include "ohos_nweb/bridge/ark_web_access_request_wrapper.h"
 #include "ohos_nweb/bridge/ark_web_app_link_callback_wrapper.h"
+#include "ohos_nweb/bridge/ark_web_color_chooser_callback_wrapper.h"
 #include "ohos_nweb/bridge/ark_web_console_log_wrapper.h"
 #include "ohos_nweb/bridge/ark_web_context_menu_callback_wrapper.h"
 #include "ohos_nweb/bridge/ark_web_context_menu_params_wrapper.h"
@@ -881,5 +882,15 @@ void ArkWebHandlerImpl::ReportDynamicFrameLossEvent(const ArkWebString& sceneId,
 void ArkWebHandlerImpl::HideHandleAndQuickMenuIfNecessary(bool hide)
 {
     nweb_handler_->HideHandleAndQuickMenuIfNecessary(hide);
+}
+
+bool ArkWebHandlerImpl::OnColorChooserShow(uint32_t initialColor, ArkWebRefPtr<ArkWebColorChooserCallback> callback)
+{
+    if (CHECK_REF_PTR_IS_NULL(callback)) {
+        return nweb_handler_->OnColorChooserShow(initialColor, nullptr);
+    }
+
+    return nweb_handler_->OnColorChooserShow(
+        initialColor, std::make_shared<ArkWebColorChooserCallbackWrapper>(callback));
 }
 } // namespace OHOS::ArkWeb
