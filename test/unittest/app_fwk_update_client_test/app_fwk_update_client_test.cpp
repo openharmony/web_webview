be93fedaa3412e51915c9673341861eead914f92
taoxueao <taoxueao1@huawei.com>
2025-04-07 11:07:02 +0000

    update test/unittest/app_fwk_update_client_test/app_fwk_update_client_test.cpp.
    Signed-off-by: taoxueao <taoxueao1@huawei.com>

--- test/unittest/app_fwk_update_client_test/app_fwk_update_client_test.cpp
+++ test/unittest/app_fwk_update_client_test/app_fwk_update_client_test.cpp
@@ -73,8 +73,8 @@ HWTEST_F(AppFwkUpdateClientTest, AppFwkUpdateClientTest_001, TestSize.Level0)
 {
     AppFwkUpdateClient &appFwkUpdateClient = AppFwkUpdateClient::GetInstance();
     EXPECT_NE(appFwkUpdateClient.appFwkUpdateDiedRecipient_, nullptr);
-    auto &clent_1 = AppFwkUpdateClient::GetInstance();
-    auto &clent_2 = AppFwkUpdateClient::GetInstance();
+    auto &client_1 = AppFwkUpdateClient::GetInstance();
+    auto &client_2 = AppFwkUpdateClient::GetInstance();
     EXPECT_EQ(&client_1, &client_2);
     EXPECT_EQ(client_1.appFwkUpdateDiedRecipient_, client_2.appFwkUpdateDiedRecipient_);
 }
