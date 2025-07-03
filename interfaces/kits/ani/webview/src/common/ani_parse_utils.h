c3f6ca0d6f00e5e3355b2811b5115aba2184f089
xiayingying <xiayingying4@huawei-partners.com>
2025-07-03 12:41:31 +0000

    update interfaces/kits/ani/webview/src/common/ani_parse_utils.h.
    Signed-off-by: xiayingying <xiayingying4@huawei-partners.com>

--- interfaces/kits/ani/webview/src/common/ani_parse_utils.h
+++ interfaces/kits/ani/webview/src/common/ani_parse_utils.h
@@ -52,7 +52,6 @@ public:
     static bool IsDouble(ani_env *env, const ani_object& object);
     static bool IsObject(ani_env *env, const ani_object& object);
     static bool CreateBoolean(ani_env *env, bool src, ani_object& aniObj);
-    static ani_object CreateInt64Object(ani_env *env, ani_long val);
     static ani_object CreateDouble(ani_env *env, ani_double val); 
     static ani_ref CreateAniStringArray(ani_env *env, const std::vector<std::string> &paths);
 };
