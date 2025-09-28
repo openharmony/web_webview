55c5d347b23ebbe40f23a2a16bf8b68e83cbc087
ohci1 <ohci1@noreply.gitcode.com>
2025-09-28 19:59:55 +0800

    https://gitcode.com/openharmony/web_webview/issues/5649
    Signed-off-by: huangji731 <huangji731@huawei.com>
    
    
    revert merge request: https://gitcode.com/openharmony/web_webview/issues/5649
    
    04d4fcd7ad17441b4404a6a743035eefba6705df
    
    This revert merge request !4460

--- ohos_interface/ohos_glue/ohos_adapter/bridge/webview/ark_hisysevent_adapter_impl.h
+++ ohos_interface/ohos_glue/ohos_adapter/bridge/webview/ark_hisysevent_adapter_impl.h
@@ -87,11 +87,6 @@ public:
     int Write(const ArkWebString& eventName, uint32_t type, const ArkWebString key1, const uint32_t value1,
         const ArkWebString key2, const uint64_t value2) override;
 
-    int Write(const ArkWebString& eventName, uint32_t type, const ArkWebString key1,
-        const int32_t value1, const ArkWebString key2, const int32_t value2, const ArkWebString key3,
-        const ArkWebString value3, const ArkWebString key4, const ArkWebString value4, const ArkWebString key5,
-        const ArkWebString value5) override;
-
 private:
     NWeb::HiSysEventAdapter& real_;
 
