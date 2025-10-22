e16cb27c63ad71837e324e1d209bc17bd11c41df
sylvieQaQ <yuanshiyu3@h-partners.com>
2025-10-22 10:28:18 +0800

    update: 更新文件 ark_hisysevent_adapter_impl.cpp
    Signed-off-by: sylvieQaQ <yuanshiyu3@h-partners.com>

--- ohos_interface/ohos_glue/ohos_adapter/bridge/webview/ark_hisysevent_adapter_impl.cpp
+++ ohos_interface/ohos_glue/ohos_adapter/bridge/webview/ark_hisysevent_adapter_impl.cpp
@@ -13,7 +13,7 @@
  * limitations under the License.
  */
 
-#include "ohos_adapter/bridge/ark_hisysevent_adapter_impl.h"
+#include "ohos_adapter/bridge/webview/ark_hisysevent_adapter_impl.h"
 
 #include "base/bridge/ark_web_bridge_macros.h"
 
