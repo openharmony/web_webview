b92bc1abe9e7a8d38972961a8f554fcfc47734a6
z30026177 <zhengyutao@huawei.com>
2025-11-18 11:00:42 +0800

    增加对huks CryptoExtension的系统能力判断
    Signed-off-by: z30026177 <zhengyutao@huawei.com>

--- interfaces/kits/napi/js/webview_export.js
+++ interfaces/kits/napi/js/webview_export.js
@@ -648,4 +648,12 @@ Object.defineProperty(webview.WebviewController.prototype, 'innerNativeMessageDi
   }
 });
 
+Object.defineProperty(webview.WebviewController.prototype, 'isHuksCryptoExtension', {
+  value: function (callback) {
+    let result = canIUse("SystemCapability.Security.Huks.CryptoExtension");
+    console.log(`zyt isHuksCryptoExtension is ${result}`);
+    return result;
+  }
+});
+
 export default webview;
