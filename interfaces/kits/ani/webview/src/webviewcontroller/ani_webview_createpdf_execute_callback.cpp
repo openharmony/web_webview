66689b22f7f20f02f13454ed2f78ba20b46ef2c4
hxf233333 <huxiaofei11@huawei.com>
2026-04-14 20:50:10 +0800

    fix: warning modify
    Signed-off-by: hxf233333 <huxiaofei11@huawei.com>

--- interfaces/kits/ani/webview/src/webviewcontroller/ani_webview_createpdf_execute_callback.cpp
+++ interfaces/kits/ani/webview/src/webviewcontroller/ani_webview_createpdf_execute_callback.cpp
@@ -185,8 +185,7 @@ static ani_object GetArrayBuffer(ani_env* env, ani_object object)
 
     for (ani_int i = 0; i < size; i++) {
         ani_int value = pdfResult[i];
-        status = env->Object_CallMethodByName_Void(
-            arrayObject, "set", "ib:", i, static_cast<ani_byte>(value));
+        status = env->Object_CallMethodByName_Void(arrayObject, "set", "ib:", i, static_cast<ani_byte>(value));
         if (status != ANI_OK) {
             WVLOG_E("arrayObject set() failed, status is %{public}d.", status);
             break;
