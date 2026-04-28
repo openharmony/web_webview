5ec30625e6631212794d00b9cdf81f4c8f1de49a
cat_fish_rain <cat_fish_rain@noreply.gitcode.com>
2026-04-28 20:16:08 +0800

    fix parseUrllist nullptr
    Signed-off-by: cat_fish_rain <lixiaoxiang0608@163.com>

--- interfaces/kits/napi/webviewcontroller/napi_webview_controller.cpp
+++ interfaces/kits/napi/webviewcontroller/napi_webview_controller.cpp
@@ -6361,6 +6361,10 @@ void NapiWebviewController::AddResourceItemToMemoryCache(napi_env env,
         BusinessError::ThrowErrorByErrcode(env, errCode);
         return;
     }
+    if (urlList.empty()) {
+        WVLOG_E("The urlList must not be empty");
+        return;
+    }
 
     std::vector<uint8_t> resource = webviewController->ParseUint8Array(env, resourceValue.resource);
     if (resource.empty() || resource.size() > MAX_RESOURCE_SIZE) {
