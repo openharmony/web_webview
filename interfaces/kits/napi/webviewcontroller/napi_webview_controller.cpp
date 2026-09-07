8902691597241bcf4cb560d80bd5af4754224a4b
Z_vento <zhangzhihao89@h-partners.com>
2026-09-07 13:39:54 +0800

    update: 更新文件 napi_webview_controller.cpp
    Signed-off-by: Z_vento <zhangzhihao89@h-partners.com>

--- interfaces/kits/napi/webviewcontroller/napi_webview_controller.cpp
+++ interfaces/kits/napi/webviewcontroller/napi_webview_controller.cpp
@@ -5517,7 +5517,7 @@ napi_value NapiWebviewController::CreateWebPrintDocumentAdapter(napi_env env, na
         BusinessError::ThrowErrorByErrcode(env, PARAM_CHECK_ERROR);
         return result;
     }
-    (void)webviewController.release();
+    (void)webPrintDocument.release();
     return proxy;
 }
 
